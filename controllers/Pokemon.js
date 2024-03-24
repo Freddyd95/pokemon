e89e83d3ea114faf38a7b86506251972df37baa3
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-24 11:14:53 -0700

    Added Pokemon

--- controllers/Pokemon.js
+++ controllers/Pokemon.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const Pokemon = require('../models/Pokemon')
+const Pokemon = require('../models/pokemon')
 
 router.get('/', async (req, res) => {
     try {
@@ -8,6 +8,17 @@ router.get('/', async (req, res) => {
     } catch (error) {
         console.log('error fetching all pokemon', error)
         res.status(500).json({ message: 'error fetching all pokemon'})
+    }
+})
+
+router.get('/:pokemonID', async (req, res) => {
+    try {
+        const { pokemonID } = req.params
+        const pokemon = await Pokemon.find({ pokemonID })
+        res.json(pokemon)
+    } catch (error) {
+        console.log('Error fetching pokemon by id', error)
+        res.status(500).json({ message: 'error fetching pokemon by id' })
     }
 })
 
